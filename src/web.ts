1df4b44174a8d4ca7bc5ebd9f31974dbfcf19dd7
Kieran Barlow <kieran.barlow@lesar.co.uk>
2024-11-11 16:07:13 +0000

    wip

--- src/web.ts
+++ src/web.ts
@@ -70,9 +70,9 @@ export class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
   private createSpeechSynthesisUtterance(
     options: TTSOptions,
   ): SpeechSynthesisUtterance {
-    //const voices = this.getSpeechSynthesisVoices();
+    const voices = this.getSpeechSynthesisVoices();
     const utterance = new SpeechSynthesisUtterance();
-    const { text, lang, rate, pitch, volume } = options;
+    const { text, lang, rate, pitch, volume, voice } = options;
     if (volume) {
       utterance.volume = volume >= 0 && volume <= 1 ? volume : 1;
     }
@@ -84,6 +84,9 @@ export class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
     }
     if (lang) {
       utterance.lang = lang;
+    }
+    if (voice) {
+      utterance.voice = voices.find(v => v.voiceURI === voice) || null;
     }
     utterance.text = text;
     return utterance;
